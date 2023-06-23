3a7e817473e9e1e7b4217d70c8b755e5339595ad
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-24 01:31:59 +0200

    Page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,69 @@
 import './styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [newDate, setNewDate] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function Submit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, date: newDate, done: false },
+      ]
+    })
+    setNewItem("")
+    setNewDate("")
+  }
+
+  function TodoDone(id, done) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, done }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={Submit}>
         <div className="form">
           <label htmlFor="item">New work</label>
-          <input type="text" id="item" />
-          <input type="date" name="deadline" id="" />
+          <input type="text" id="item" value={newItem} onChange={e => setNewItem(e.target.value)} required />
+          <input type="date" name="deadline" value={newDate} onChange={e => setNewDate(e.target.value)} id="date" />
           <button>Add</button>
         </div>
       </form>
       <section>
-      <h1 className='header'>Todo List</h1>
+        <h1 className='header'>Todo List</h1>
         <div className="list">
           <ul>
-            <li>
-              <label>
-                <input type="checkbox" />
-                Take a dog for a walk wigh maggie 2023/23/06
-              </label>
-              <FontAwesomeIcon icon={faTrash} className='faTrash'></FontAwesomeIcon>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" />
-                Item 2 2023/26/02
-                <FontAwesomeIcon icon={faTrash} className='faTrash'></FontAwesomeIcon>
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" />
-                Item 2 2023/26/02
-                <FontAwesomeIcon icon={faTrash} className='faTrash'></FontAwesomeIcon>
-              </label>
-            </li>
+            {todos.length === 0 && "No Todos"}
+            {todos.map(todo => {
+              return (
+                <li key={todo.id}>
+                  <label>
+                    <input type="checkbox" checked={todo.done} onChange={e => TodoDone(todo.id, e.target.checked)} />
+                    {todo.title} {todo.date}
+                  </label>
+                  <FontAwesomeIcon onClick={() => deleteTodo(todo.id)} icon={faTrash} className='faTrash'></FontAwesomeIcon>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </section>
