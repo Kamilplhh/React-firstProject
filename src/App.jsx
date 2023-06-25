a8eefe5a80e03c62783144760226aa74ec9ee6b1
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-25 17:09:46 +0200

    Page progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { useState } from 'react';
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
-  const [newDate, setNewDate] = useState("")
   const [todos, setTodos] = useState([])
+  const current = new Date();
+  const today = `${current.getFullYear()}-0${current.getMonth() + 1}-${current.getDate()}`;
+  const [newDate, setNewDate] = useState(today)
 
   function Submit(e) {
     e.preventDefault()
@@ -18,7 +20,7 @@ export default function App() {
       ]
     })
     setNewItem("")
-    setNewDate("")
+    setNewDate(today)
   }
 
   function TodoDone(id, done) {
@@ -38,35 +40,40 @@ export default function App() {
     })
   }
 
+  let nextweek = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+  nextweek = `${nextweek.getFullYear()}-0${nextweek.getMonth() + 1}-${nextweek.getDate()}`;
+
   return (
-    <>
-      <form onSubmit={Submit}>
-        <div className="form">
-          <label htmlFor="item">New work</label>
-          <input type="text" id="item" value={newItem} onChange={e => setNewItem(e.target.value)} required />
-          <input type="date" name="deadline" value={newDate} onChange={e => setNewDate(e.target.value)} id="date" />
-          <button>Add</button>
-        </div>
-      </form>
-      <section>
-        <h1 className='header'>Todo List</h1>
-        <div className="list">
-          <ul>
-            {todos.length === 0 && "No Todos"}
-            {todos.map(todo => {
-              return (
-                <li key={todo.id}>
-                  <label>
-                    <input type="checkbox" checked={todo.done} onChange={e => TodoDone(todo.id, e.target.checked)} />
-                    {todo.title} {todo.date}
-                  </label>
-                  <FontAwesomeIcon onClick={() => deleteTodo(todo.id)} icon={faTrash} className='faTrash'></FontAwesomeIcon>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </section>
-    </>
-  )
-}
+        <>
+          <form onSubmit={Submit}>
+            <div className="form">
+              <label htmlFor="item">New work</label>
+              <input type="text" id="item" value={newItem} onChange={e => setNewItem(e.target.value)} required />
+              <input type="date" name="deadline" value={newDate} onChange={e => setNewDate(e.target.value)} id="date" min={today} />
+              <button>Add</button>
+            </div>
+          </form>
+          <section>
+            <h1 className='header'>Todo List</h1>
+            <div className="list">
+              <ul>
+                {todos.length === 0 && "No Todos"}
+                {todos.map(todo => {
+                  return (
+                    <li key={todo.id}>
+                      <label>
+                        <input type="checkbox" checked={todo.done} onChange={e => TodoDone(todo.id, e.target.checked)} />
+                        {todo.title} {todo.date}
+                      </label>
+                      <FontAwesomeIcon onClick={() => deleteTodo(todo.id)} icon={faTrash} className='faTrash'></FontAwesomeIcon>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </section>
+          {/* <button onClick={() => weeklyTodo()}>This week</button> */}
+          {/* <button onClick={() => allTodo()}>Rest</button> */}
+        </>
+      )
+    }
